b8906d7eaaefc64b8040e850a7188100b83f9be4
poonsap16 <kengp16@hotmail.com>
2017-10-06 16:26:26 -0700

    test study

--- app/RfidData.php
+++ app/RfidData.php
@@ -3,8 +3,32 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class RfidData extends Model
 {
-    //
+    protected $table = 'rfid_data';
+
+    protected $fillable = [
+	    	  'id',
+	    	  'rfid_number',
+	        'sap_id',
+	        'firstname_lastname',
+	        'date_stamp',
+	        'time_stamp',
+	        'reader_location',
+	        'reader_status',
+	        'reader_door'
+    ];
+
+    public static function loadData($fileName)
+    {
+      $rfidRecords = loadCSV($fileName);
+
+      foreach ($rfidRecords as $rfidRecord) {
+          $rfidRecord['date_stamp'] = Carbon::createFromFormat('m/d/Y', $rfidRecord['date_stamp'])->toDateString();
+          RfidData::create($rfidRecord);
+      }
+
+    }
 }
