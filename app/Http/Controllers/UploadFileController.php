01173532f397c834f42a7ae2333b658c72038814
poonsap16 <kengp16@hotmail.com>
2017-10-20 16:20:17 -0700

    update test upload code :

--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -9,7 +9,24 @@ class UploadFileController extends Controller
     public function uploadFile(Request $request){
         //เช็คว่า $request input ที่ชื่อว่า 'files' เป็นไฟล์หรือไม่
         if ($request->hasFile('files')){
-          return "yes";
+
+          foreach($request->file('files') as $file){
+            if ($file->isValid()) {
+              // return "file is valid";
+              $filePaths[] = $file->store('uploads');
+              //
+              // $data = loadCSVFromUpload($filePaths[0]);
+              //return $data;
+            }
+          }
+
+            foreach($filePaths as $filePath){
+            //load data from file to table
+              //$data = loadCSVFromUpload($filePath);
+            return "yes";
+            // delete file
+              Storage::delete($filePath);
+            }
         }
           return "no";
     }
