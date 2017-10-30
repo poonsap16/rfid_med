e753a34d3afa21c47df7201b4709ca98132bfa45
poonsap16 <kengp16@hotmail.com>
2017-10-30 11:28:24 -0700

    index page

---
+++ resources/views/rfid_index.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Rfid Medicine Record</title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+    <style>
+        html, body {
+            font-family: 'Lato', Arial;
+        }
+        body {
+            padding-top: 30px;
+        }
+        li.active {
+            color: #666663!important;
+            border-color: #E8E8E8!important;
+            background-color: #fffff6!important;
+            text-align: center;
+            font-weight: bold;
+            font-style: italic;
+        }
+    </style>
+
+  </head>
+  <body>
+
+    <nav class="navbar navbar-default">
+      <!-- ส่วนหัวของ application -->
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="/rfid">RFID Medicine Record</a>
+        </div>
+        </nav>
+        <nav>
+        <!-- รายการเมนูของ application -->
+        <div>
+          <ul class="nav nav-justified">
+          <li><a href="/uploadfile">โหลดข้อมูล</a></li>
+          <li><a href="#">รายการกิจกรรม</a></li>
+          <li><a href="#">จัดตารางกิจกรรม</a></li>
+          <li><a href="#">รายงาน</a></li>
+          <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+      </div>
+    </nav>
+
+
+
+  </body>
+</html>
