3af89923984661aaa9af0ea1a82580ab58b0ebe5
poonsap16 <kengp16@hotmail.com>
2017-11-02 20:22:30 +0700

    test

--- resources/views/rfid_index.blade.php
+++ resources/views/rfid_index.blade.php
@@ -58,7 +58,7 @@
             <li><a href="#">test</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Username</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign In</a></li>
             <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
           </ul>
         </div>
